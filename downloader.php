6eb3ee98b1f05639a3561ea6bf96a980c729c32c
jesusAG <33084455+jesusAG28@users.noreply.github.com>
2023-08-10 12:24:52 +0200

    update

--- downloader.php
+++ downloader.php
@@ -11,13 +11,21 @@ if (!is_dir('./templates'))
     mkdir('./templates', 0775, true);
 
 
-$count = 0;
-$time = microtime(true);
+$ok         = 0;
+$errorCount = 0;
+$errors     = [];
+$time       = microtime(true);
+$latestPage = 294;
 
-for ($i = 290; $i <= 290; $i--) {
-    $page = BASE_URL . 'free-css-templates/page' . $i;
+for ($i = $latestPage; $i <= $latestPage; $i--) {
+    $page = BASE_URL . 'free-css-templates/page' . $i . '/';
 
-    $dom->loadFromFile($page);
+    try {
+        $dom->loadFromFile($page);
+    } catch (Exception $e) {
+        $errorCount++;
+        continue;
+    }
 
     $showcase = $dom->find('#showcase');
 
@@ -35,34 +43,56 @@ for ($i = 290; $i <= 290; $i--) {
         $a = $li->firstChild()->find('a');
 
         $title = trim(str_replace('Website Template', '', $a->getAttribute('title')));
-        $title = str_replace(' ', '_', $title);
+        $title = str_replace([' ', '.'], '_', $title);
 
         $template_url = str_replace('free-css-templates/', 'free-css-templates/download/', $a->getAttribute('href'));
         $template_url = str_replace('//', '/', BASE_URL . 'assets/files/' . $template_url);
 
-        echo $template_url . '<br>';
+        echo $template_url;
 
         if (!is_dir('./templates/' . $title))
             mkdir('./templates/' . $title, 0775, true);
 
         // $res = file_put_contents('./templates/' . $title . '/' . $title . '.zip', fopen($template_url . '.zip', 'r'));
 
-        $ch = curl_init();
+        $ch     = curl_init();
         $source = $template_url;
         curl_setopt($ch, CURLOPT_URL, $source);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec($ch);
         curl_close($ch);
 
-        $destination = './templates/' . $title . '/' . $title . '.zip';
-        $file = fopen($destination, "w+");
-        fputs($file, $data);
-        fclose($file);
+        try {
+            $destination = './templates/' . $title . '/' . $title . '.zip';
+            $file        = fopen($destination, "w+");
+
+            fputs($file, $data);
+            fclose($file);
+            $ok++;
+
+            echo PHP_EOL;
+            echo 'OK';
+            echo PHP_EOL;
+            echo PHP_EOL;
+        } catch (Exception $e) {
+            $errors[] = $source;
+            echo PHP_EOL;
+            echo 'ERROR';
+            echo PHP_EOL;
+            echo PHP_EOL;
+        }
+
+        echo 'OK -> ' . $ok . ' ERR -> ' . count($errors);
+        echo PHP_EOL;
+        echo PHP_EOL;
 
     }
 
 }
 
-$end = microtime(true);
+$end  = microtime(true);
 $time = $end - $start;
+echo PHP_EOL;
+echo PHP_EOL;
 echo "Tiempo de ejecución: (<i>$time</i>) segundos";
+echo PHP_EOL;
---
+++ redme.md
@@ -0,0 +1,1 @@
+required comand composer install
